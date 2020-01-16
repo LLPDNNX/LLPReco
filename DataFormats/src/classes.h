63f88cdba33a192d3e96668c766cf5b5fb4a19c2
Haifa Rejeb Sfar <haifa.rejeb.sfar@cern.ch>
2020-01-16 13:39:49 +0100

    resolving conflict

--- DataFormats/src/classes.h
+++ DataFormats/src/classes.h
@@ -17,8 +17,40 @@ namespace {
         llpdnnx::SecondaryVertexFeatures dummy6;
         llpdnnx::ChargedCandidateFeatures dummy7;
         llpdnnx::NeutralCandidateFeatures dummy8;
-        llpdnnx::ElectronCandidateFeatures dummy9;
-        llpdnnx::MuonCandidateFeatures dummy10;
+	
+        llpdnnx::DisplacedGenVertexCollection dummy9;
+        edm::Wrapper<llpdnnx::DisplacedGenVertexCollection> dummy10;
+        
+        edm::Ptr<llpdnnx::DisplacedGenVertex> dummy11;
+        edm::Wrapper<edm::Ptr<llpdnnx::DisplacedGenVertex>> dummy12;
+        
+        edm::Ptr<llpdnnx::DisplacedGenVertexCollection> dummy13;
+        edm::Wrapper<edm::Ptr<llpdnnx::DisplacedGenVertexCollection>> dummy14;
+
+        edm::PtrVector<llpdnnx::DisplacedGenVertexCollection> dumm15;
+        edm::Wrapper<edm::PtrVector<llpdnnx::DisplacedGenVertexCollection>> dummy16;
+        
+        edm::PtrVector<reco::GenParticle> dummy17;
+        edm::Wrapper<edm::PtrVector<reco::GenParticle>> dummy18;
+        
+        llpdnnx::LLPLabel dummy19;
+        reco::FeaturesTagInfo<llpdnnx::LLPLabel> dummy20;
+        std::vector<reco::FeaturesTagInfo<llpdnnx::LLPLabel>> dummy21;
+        edm::Wrapper<std::vector<reco::FeaturesTagInfo<llpdnnx::LLPLabel>>> dummy22;
+        
+        llpdnnx::LLPGenDecayInfo dummy23;
+        std::vector<llpdnnx::LLPGenDecayInfo> dummy24;
+        edm::Wrapper<std::vector<llpdnnx::LLPGenDecayInfo>> dummy25;
+        
+        llpdnnx::LLPGhostFlavourInfo dummy26;
+        std::vector<llpdnnx::LLPGhostFlavourInfo> dummy27;
+        edm::Wrapper<std::vector<llpdnnx::LLPGhostFlavourInfo>> dummy28;
+        edm::ValueMap<llpdnnx::LLPGhostFlavourInfo> dummy29;
+        edm::Wrapper<edm::ValueMap<llpdnnx::LLPGhostFlavourInfo>> dummy30;
+
+
+        llpdnnx::ElectronCandidateFeatures dummy31;
+        llpdnnx::MuonCandidateFeatures dummy32;
 
     };
 
