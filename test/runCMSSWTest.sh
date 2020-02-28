c79a6e821aa7a2c1cf1aac6917215e4ba96156d0
daiktas <vilius.cepaitis@gmail.com>
2020-02-28 19:02:18 +0100

    fix

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -1,14 +1,12 @@
 function run_test()
 {
     cd ~ 
-    source /cvmfs/cern.ch/cmsset_default.sh
+    source /cvmfs/cms.cern.ch/cmsset_default.sh
     export SCRAM_ARCH=slc6_amd64_gcc700 || return 1
     scramv1 project CMSSW CMSSW_10_2_18 || return 1
     cd CMSSW_10_2_18/src || return 1
     eval `scramv1 runtime -sh` || return 1
     scram b || return 1
-     
 }
-
 
 run_test
