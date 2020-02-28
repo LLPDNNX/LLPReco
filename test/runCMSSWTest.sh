9898f337813a06649bed030f77723fd6902e923d
Vilius Cepaitis <vilius.cepaitis@gmail.com>
2020-02-28 17:09:13 +0100

    Update runCMSSWTest.sh

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -1,8 +1,5 @@
 function run_test()
 {
-    touch /var/lib/rpm/* || return 1
-    yum -y install wget rsync yum-plugin-ovl || return 1
-    yum -y install glibc-devel.x86_64 --disablerepo=adobe* || return 1
     cd ~
     source ~/.bashrc
     export SCRAM_ARCH=slc6_amd64_gcc630 || return 1
