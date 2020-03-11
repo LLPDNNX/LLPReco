49c1a7bbacd68f759267c50ae57c7853e94e3205
daiktas <vilius.cepaitis@gmail.com>
2020-03-11 15:11:26 +0100

    downgrade

--- test/runCMSSWTest.sh
+++ test/runCMSSWTest.sh
@@ -3,9 +3,9 @@ function run_test()
     cd ~
     source /cvmfs/cms.cern.ch/cmsset_default.sh
     export SCRAM_ARCH=slc6_amd64_gcc700 || return 1
-    scramv1 project CMSSW CMSSW_10_2_15 || return 1
-    cd CMSSW_10_2_15/src || return 1
-    eval `scramv1 runtime -sh` || return 1
+    scramv1 project CMSSW CMSSW_10_2_18 || return 1
+    cd CMSSW_10_2_18/src || return 1
+    eval `scram runtime -sh` || return 1
     rsync -r /scripts/* LLPReco  || return 1
     scram b || return 1
     pip install --user -r LLPReco/requirements.txt || return 1
